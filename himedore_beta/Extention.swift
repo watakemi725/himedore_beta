2f4e7064852b5963b0222ba3d6f987374dca3cf0
Takemi Watanuki <takemi007@hotmail.com>
2015-02-26 16:05:59 +0900

    shuffle func

---
+++ himedore_beta/Extention.swift
@@ -0,0 +1,28 @@
+//
+//  Extention.swift
+//  himedore_beta
+//
+//  Created by Takemi Watanuki on 2015/02/26.
+//  Copyright (c) 2015年 watakemi725. All rights reserved.
+//
+
+import Foundation
+
+extension Array {
+    mutating func shuffle(count: Int) {
+        for _ in 0..<count {
+            sort { (_,_) in arc4random() < arc4random() }
+        }
+    }
+}
+
+extension NSMutableArray {
+    func shuffle(count: Int) {
+        for i in 0..<count {
+            var nElements: Int = count - i
+            var n: Int = Int(arc4random_uniform(UInt32(nElements))) + i
+            self.exchangeObjectAtIndex(i, withObjectAtIndex: n)
+        }
+    }
+    
+}
--- himedore_beta/SettingViewController.swift
+++ himedore_beta/SettingViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+
+
 class SettingViewController: UIViewController {
     
     @IBOutlet var allNumlabel : UILabel!
@@ -16,17 +18,24 @@ class SettingViewController: UIViewController {
     var allNum = 0
     var oujiNum = 0
     
+    var exampleArray: NSMutableArray = NSMutableArray()
     
     
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         
         
         
+        exampleArray = ["hoge", "huga", "piyo"]
+        exampleArray.shuffle(exampleArray.count) //外部ファイルの.shuffleで用意したファイルの数字をシャッフルしてくれる
         
+        for i in 0...(exampleArray.count)-1 {
+            println("\(exampleArray[i])")
+        }
         
         //入力された値から役割とか計算する
         
@@ -36,22 +45,31 @@ class SettingViewController: UIViewController {
         
         
     }
-
+    
     @IBAction func plusAllNum(){
         allNum++
-        allNumlabel.text="\(allNum)"
+        allNumlabel.text="\(allNum)人"
     }
     @IBAction func minusAllNum(){
-        allNum--
-        allNumlabel.text="\(allNum)"
+        if(allNum>0){
+            allNum--
+        }
+        allNumlabel.text="\(allNum)人"
+        
     }
     @IBAction func plusOujiNum(){
         oujiNum++
-        oujiNumlabel.text="\(oujiNum)"
+        oujiNumlabel.text="\(oujiNum)人"
     }
     @IBAction func minusOujiNum(){
-        oujiNum--
-        oujiNumlabel.text="\(oujiNum)"
+        if(oujiNum>1){
+            oujiNum--
+        }
+        oujiNumlabel.text="\(oujiNum)人"
+    }
+    
+    @IBAction func start(){
+    
     }
     
     
@@ -61,15 +79,15 @@ class SettingViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
