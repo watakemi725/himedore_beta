1879f053e647a6a5d7c2ab5bce5f913f3cce7351
Takemi Watanuki <takemi007@hotmail.com>
2015-02-26 15:06:55 +0900

    +++

--- himedore_beta/SettingViewController.swift
+++ himedore_beta/SettingViewController.swift
@@ -9,6 +9,15 @@
 import UIKit
 
 class SettingViewController: UIViewController {
+    
+    @IBOutlet var allNumlabel : UILabel!
+    @IBOutlet var oujiNumlabel : UILabel!
+    
+    var allNum = 0
+    var oujiNum = 0
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +37,25 @@ class SettingViewController: UIViewController {
         
     }
 
+    @IBAction func plusAllNum(){
+        allNum++
+        allNumlabel.text="\(allNum)"
+    }
+    @IBAction func minusAllNum(){
+        allNum--
+        allNumlabel.text="\(allNum)"
+    }
+    @IBAction func plusOujiNum(){
+        oujiNum++
+        oujiNumlabel.text="\(oujiNum)"
+    }
+    @IBAction func minusOujiNum(){
+        oujiNum--
+        oujiNumlabel.text="\(oujiNum)"
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
