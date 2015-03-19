98c8d8885b9496464b65ec3e8dd16275283d7eea
Takemi Watanuki <takemi007@hotmail.com>
2015-03-20 02:49:43 +0900

    +++

--- himedore_beta/HaiyakuViewController.swift
+++ himedore_beta/HaiyakuViewController.swift
@@ -13,8 +13,8 @@ class HaiyakuViewController: UIViewController {
     
     @IBOutlet var hakoLabel : UILabel!
     @IBOutlet var yakuLabel : UILabel!
-        @IBOutlet var junLabel : UILabel!
-
+    @IBOutlet var junLabel : UILabel!
+    
     //NSUserDefaultsのインスタンスを生成
     let playerHokan = NSUserDefaults.standardUserDefaults()
     
@@ -27,12 +27,12 @@ class HaiyakuViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         
         //何番目か確認する
         //nsuserdefaultsで配役の配列はあたえていいんじゃないの
-         junbanNum = appDelegate.turnNum
+        junbanNum = appDelegate.turnNum
         
         //前回の保存内容があるかどうかを判定
         if((playerHokan.objectForKey("player")) != nil){
@@ -57,15 +57,10 @@ class HaiyakuViewController: UIViewController {
             yakuLabel.text = "\(playerArray[junbanNum])"
             
             
-            
         }
         
-
-   
-        
-        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -74,37 +69,39 @@ class HaiyakuViewController: UIViewController {
     
     @IBAction func complete(){
         //元の画面にもどるか、最後のプレーヤーだったらタイマーを開始するように反手居るしてあげる
-
+        
         if (playerArray.count==junbanNum+1 ){
             //おわり　タイマーへ
             println("おわりんこ")
-                        var targetView: AnyObject = self.storyboard!.instantiateViewControllerWithIdentifier( "timer" )
-            self.presentViewController( targetView as UIViewController, animated: true
+            var targetView: AnyObject = self.storyboard!.instantiateViewControllerWithIdentifier( "timer" )
+            self.presentViewController( targetView as UIViewController, animated: false
                 , completion: nil)
-            
+            self.dismissViewControllerAnimated(true, completion: nil)
         }else{
             appDelegate.turnNum = junbanNum + 1  //appDelegateの変数を操作 順番かえる
             
             var targetView: AnyObject = self.storyboard!.instantiateViewControllerWithIdentifier( "haiyaku" )
             
-            self.presentViewController( targetView as UIViewController, animated: true
+            self.presentViewController( targetView as UIViewController, animated: false
                 , completion: nil)
+            //けす
+        self.dismissViewControllerAnimated(true, completion: nil)
         }
         
         
     }
     
-
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
     
     
-
+    
 }
--- himedore_beta/SettingViewController.swift
+++ himedore_beta/SettingViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 
 
-class SettingViewController: UIViewController {
+class SettingViewController: UIViewController ,UIAlertViewDelegate{
     
     @IBOutlet var allNumlabel : UILabel!
     @IBOutlet var oujiNumlabel : UILabel!
@@ -77,51 +77,112 @@ class SettingViewController: UIViewController {
     @IBAction func start(){
         dorei = allNum - oujiNum
         
-        if(dorei%2==0){
-            k = (dorei/2)-1
+        if(dorei>2){
+            
+            if(dorei%2==0){
+                k = (dorei/2)-1
+            }else{
+                k=dorei/2
+            }
+            
+            yoiDorei = Int(arc4random() % UInt32(k) + 1) //キャスト
+            println("良い奴隷は\(yoiDorei)人")
+            waruiDorei = dorei - yoiDorei
+            println("悪い奴隷は\(waruiDorei  )人")
+            
+            for i in 0..<oujiNum{
+                playerArray.addObject("王子")
+            }
+            for i in 0..<(waruiDorei){
+                playerArray.addObject("悪奴隷")
+            }
+            for i in 0..<(yoiDorei){
+                playerArray.addObject("良い奴隷")
+            }
+            
+            //        playerArray = ["hoge", "huga", "piyo"]
+            playerArray.shuffle(playerArray.count) //外部ファイルの.shuffleで用意したファイルの数字をシャッフルしてくれる
+            
+            for i in 0...(playerArray.count)-1 {
+                println(" \(i+1)人目は\(playerArray[i])")
+            }
+            
+            //配列をnsuserdefaultsに入れる
+            
+            //NSUserDefaultsのインスタンスを生成
+            let playerHokan = NSUserDefaults.standardUserDefaults()
+            
+            //"NAME"というキーで配列namesを保存
+            playerHokan.setObject(playerArray, forKey:"player")
+            
+            // シンクロを入れないとうまく動作しないときがあります
+            playerHokan.synchronize()
+            
+            
+            appDelegate.turnNum = 1 //appDelegateの変数を操作
+            
+            
+            
+            
+            playerArray.removeAllObjects()
+            
+            
+            var targetView: AnyObject = self.storyboard!.instantiateViewControllerWithIdentifier( "haiyaku" )
+            
+            self.presentViewController( targetView as UIViewController, animated: false
+                , completion: nil)
+            
         }else{
-            k=dorei/2
+            //数が足らんor おかしいやろ
+            
+            println("だめーーー")
+            
+            func alertView(alertView: UIAlertView, clickedButtonAtIndex buttonIndex: Int) {
+                if (buttonIndex == alertView.cancelButtonIndex) {
+                    //Canceled
+                    println("たんねーのかよーわかったよー")
+                    
+                    //画面遷移も何もせずにとどまる
+                }
+            
+            }
+
+            
+            if objc_getClass("UIAlertController") != nil {
+                //UIAlertController使用
+                //アラート使うならここも必要らしいぞ!!
+                
+                var ac = UIAlertController(title: "Title", message: "Message", preferredStyle: .Alert)
+                
+                let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel) { (action) -> Void in
+                    println("Cancel button tapped.")
+                }
+                
+                let okAction = UIAlertAction(title: "OK", style: .Default) { (action) -> Void in
+                    println("OK button tapped.")
+                }
+                
+                ac.addAction(cancelAction)
+                ac.addAction(okAction)
+                
+                presentViewController(ac, animated: true, completion: nil)
+                
+                
+                
+            } else {
+                //UIAlertView使用
+                var av = UIAlertView(title: "警告", message:"王子の数を減らすか、\n全プレーヤー数を増やしてください", delegate: self, cancelButtonTitle: "OK")
+                av.show()
+            }
+            
+            
+    
+            
+            
+            
         }
-        yoiDorei = Int(arc4random() % UInt32(k) + 1) //キャスト
-        println("良い奴隷は\(yoiDorei)人")
-        waruiDorei = dorei - yoiDorei
-        println("悪い奴隷は\(waruiDorei  )人")
-        
-        for i in 0..<oujiNum{
-            playerArray.addObject("王子")
-        }
-        for i in 0..<(waruiDorei){
-            playerArray.addObject("悪奴隷")
-        }
-        for i in 0..<(yoiDorei){
-            playerArray.addObject("良い奴隷")
-        }
-        
-        //        playerArray = ["hoge", "huga", "piyo"]
-        playerArray.shuffle(playerArray.count) //外部ファイルの.shuffleで用意したファイルの数字をシャッフルしてくれる
-        
-        for i in 0...(playerArray.count)-1 {
-            println(" \(i+1)人目は\(playerArray[i])")
-        }
-        
-        //配列をnsuserdefaultsに入れる
-        
-        //NSUserDefaultsのインスタンスを生成
-        let playerHokan = NSUserDefaults.standardUserDefaults()
-        
-        //"NAME"というキーで配列namesを保存
-        playerHokan.setObject(playerArray, forKey:"player")
-        
-        // シンクロを入れないとうまく動作しないときがあります
-        playerHokan.synchronize()
         
         
-        appDelegate.turnNum = 1 //appDelegateの変数を操作
-        
-        
-        
-        
-        playerArray.removeAllObjects()
     }
     
     
